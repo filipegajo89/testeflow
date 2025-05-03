21a06651268a83db74d2297303e2887398f9af7a
filipegajo89 <filipegajo89@gmail.com>
2025-05-03 10:40:26 -0300

    Create appwrite-config.js

---
+++ js/appwrite-config.js
@@ -0,0 +1,18 @@
+// appwrite-config.js
+const client = new Appwrite.Client();
+
+// Configure sua instância do Appwrite Cloud
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('SEU-PROJECT-ID'); // Substitua pelo ID do seu projeto
+
+// Cria os serviços
+const account = new Appwrite.Account(client);
+const databases = new Appwrite.Databases(client);
+const storage = new Appwrite.Storage(client);
+
+// Configuração de banco de dados
+const DATABASE_ID = 'flowbnb-database';
+const USERS_COLLECTION_ID = 'users';
+const PROPERTIES_COLLECTION_ID = 'properties';
+const TRANSACTIONS_COLLECTION_ID = 'transactions';
